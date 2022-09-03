069c5b4ce39cbf391939b2afdcd662ec43d1a18f
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-09-02 18:09:35 -0600

    Created trigger enter event script

---
+++ MatchGame2022/Assets/Scripts/TriggerEventBehavior.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+}
