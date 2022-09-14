5e75380c71c396bbc7952c7fedeeb8ffc66bec33
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-09-14 17:58:08 -0600

    Created animations for the square

--- MatchGame2022/Assets/Scripts/DraggableBehavior.cs
+++ MatchGame2022/Assets/Scripts/DraggableBehavior.cs
@@ -1,12 +1,14 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class DraggableBehavior : MonoBehaviour
 {
     private Camera cameraObj;
     public bool draggable;
+    public Vector3 position, offset;
+    public UnityEvent startDragEvent, endDragEvent;
     
     void Start()
     {
@@ -17,13 +19,22 @@ public class DraggableBehavior : MonoBehaviour
      which detects when you click an object with the draggable attribute.*/
     public IEnumerator OnMouseDown()
     {
+        
+        offset = transform.position - cameraObj.ScreenToWorldPoint(Input.mousePosition);
+        yield return new WaitForFixedUpdate();
+        
         draggable = true;
+        
+        startDragEvent.Invoke();
+        
         while (draggable)
         {
             // We use WaitForFixedUpdate instead of the Update() function.
             yield return new WaitForFixedUpdate();
-            // now we have to tell the object to do something
-            Debug.Log("Drag");
+            // now we tell it to get the position of the mouse
+            position = cameraObj.ScreenToWorldPoint(Input.mousePosition) + offset;
+            // then tell the object to move to that position
+            transform.position = position;
         }
     }
     
@@ -31,6 +42,7 @@ public class DraggableBehavior : MonoBehaviour
     private void OnMouseUp()
     {
         draggable = false;
+        endDragEvent.Invoke();
     }
     
 }
