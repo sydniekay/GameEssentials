55318cf315d322f9d0c7252d4f6d9c418691cf1d
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-09-20 21:21:12 -0600

    Added countdown UI with animations, and ring and dot sprite

---
+++ MatchGame2022/Assets/Scripts/CoroutineBehavior.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehavior : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    
+    public IntData counterNum;
+    
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+
+    private WaitForFixedUpdate wffuObj;
+    IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+        
+        startEvent.Invoke();
+
+        while (counterNum.value > 0)
+        {
+            repeatEvent.Invoke();
+            counterNum.value--;
+            yield return wfsObj;
+        }
+        endEvent.Invoke();
+    }
+}
