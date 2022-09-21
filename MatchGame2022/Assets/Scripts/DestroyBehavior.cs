5ffe729a8c34dd38f2ca94076c2fe2e3d3ec0bc7
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-09-21 15:47:24 -0600

    Created Vector3List and DestroyBehavior

---
+++ MatchGame2022/Assets/Scripts/DestroyBehavior.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBehavior : MonoBehaviour
+{
+    public float seconds = 1;
+    private WaitForSeconds wfsObj;
+    
+    IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        yield return wfsObj;
+        Destroy(gameObject);
+    }
+    
+    
+}
--- MatchGame2022/Assets/Scripts/Instancer.cs
+++ MatchGame2022/Assets/Scripts/Instancer.cs
@@ -1,10 +1,40 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class Instancer : ScriptableObject
 {
-    public void CreateInstance(GameObject obj)
+    public GameObject prefab;
+    private int num;
+    
+    public void CreateInstance()
     {
-        Instantiate(obj);
+        Instantiate(prefab);
+    }
+
+    public void CreateInstance(Vector3Data obj)
+    {
+        Instantiate(prefab, obj.value, Quaternion.identity);
+    }
+
+    public void CreateInstanceFromList(Vector3DataList obj)
+    {
+        foreach (var t in obj.vector3List)
+        {
+            Instantiate(prefab, t.value, Quaternion.identity);
+        }
+    }
+    
+    
+    
+    public void CreateInstanceFromListCounting(Vector3DataList obj)
+    {
+        // Instantiate at zero, increment, once it hits the number of items in the list, it will reset.
+        Instantiate(prefab, obj.vector3List[num].value, Quaternion.identity);
+        num++;
+        if (num == obj.vector3List.Count)
+        {
+            num = 0;
+        }
     }
 }
